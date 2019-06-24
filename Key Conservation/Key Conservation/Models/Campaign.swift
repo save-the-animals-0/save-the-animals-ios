bf596ff6b2e37a12d68cb350e2012c3997b29186
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-24 19:13:14 -0400

    Added models/controllers and started building out required methods

---
+++ Key Conservation/Key Conservation/Models/Campaign.swift
@@ -0,0 +1,20 @@
+//
+//  Campaign.swift
+//  Key Conservation
+//
+//  Created by Sean Acres on 6/24/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+
+struct Campaign: Codable {
+    let title: String
+    let location: String
+    let description: String
+    let imageURL: String
+    let fundingRaised: String
+    let fundingGoal: String
+    let deadline: String
+    let category: String
+}
---
+++ Key Conservation/Key Conservation/Models/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  Key Conservation
+//
+//  Created by Sean Acres on 6/24/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import Foundation
+
+struct User: Codable {
+    let name: String
+    let password: String
+    let email: String
+    let imageURL: String
+    // let campaignsSupported: [String]
+}
