3684b20e235461d869ab091b5f08da6364d7102a
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-24 16:35:15 -0400

    Set up launch screen, get started, and sign up views/files

---
+++ Key Conservation/Key Conservation/View Controllers/GetStartedViewController.swift
@@ -0,0 +1,38 @@
+//
+//  GetStartedViewController.swift
+//  Key Conservation
+//
+//  Created by Sean Acres on 6/24/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class GetStartedViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        for family: String in UIFont.familyNames
+        {
+            print(family)
+            for names: String in UIFont.fontNames(forFamilyName: family)
+            {
+                print("== \(names)")
+            }
+        }
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
