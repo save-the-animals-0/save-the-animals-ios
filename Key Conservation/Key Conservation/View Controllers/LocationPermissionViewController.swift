917565495cec7b5be4f9cca961cb6f401d9ca292
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-24 16:46:45 -0400

    Added location permission screen

---
+++ Key Conservation/Key Conservation/View Controllers/LocationPermissionViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LocationPermissionViewController.swift
+//  Key Conservation
+//
+//  Created by Sean Acres on 6/24/19.
+//  Copyright © 2019 Sean Acres. All rights reserved.
+//
+
+import UIKit
+
+class LocationPermissionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
