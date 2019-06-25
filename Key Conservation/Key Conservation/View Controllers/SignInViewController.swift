348b81d50658fa30b0d5d4b7a4cf06b0ce23f6c5
seanacres <6416690+seanacres@users.noreply.github.com>
2019-06-25 18:13:34 -0400

    Set up sign in view

--- Key Conservation/Key Conservation/View Controllers/SignInViewController.swift
+++ Key Conservation/Key Conservation/View Controllers/SignInViewController.swift
@@ -12,11 +12,18 @@ class SignInViewController: UIViewController {
 
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    private let userController = UserController()
+    var user: User?
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "ShowFeed" {
+            guard let campaignTableVC = segue.destination as? CampaignTableViewController else { return }
+            campaignTableVC.userController = userController
+        }
     }
     
     @IBAction func backButtonTapped(_ sender: Any) {
@@ -24,6 +31,23 @@ class SignInViewController: UIViewController {
     }
     
     @IBAction func signInTapped(_ sender: Any) {
+        guard let email = emailTextField.text,
+            email != "",
+            let password = passwordTextField.text,
+            password != "" else { return }
+        user = User(name: nil, password: password, email: email, imageURL: nil, imageData: nil, type: nil)
+        performSegue(withIdentifier: "ShowFeed", sender: self)
+        //        userController.loginWith(user: user!, loginType: .signIn) { (error) in
+        //            if let error = error {
+        //                print(error)
+        //                return
+        //            }
+        //
+        //            DispatchQueue.main.async {
+        //                self.performSegue(withIdentifier: "ShowFeed", sender: self)
+        //            }
+        //        }
+        
     }
     
 }
